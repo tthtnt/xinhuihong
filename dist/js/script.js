f58d7ceb994a33b390f47f2dd9392701c50d3c0e
tthtnt <2420313474@qq.com>
2018-07-31 17:06:17 +0800

    眼镜和康复系列完成

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,10 +2,13 @@
 var _index7=0;
 $(".flash4 ul li").mouseover(function(){
 	_index7=$(this).index();
-	$(this).stop().stop().animate({width:538},500).siblings("li").stop().animate({width:106},500);
-	$(".imgCen").eq(_index7).css("display","block").siblings(".imgCen").css("display","none");
-	$("p.bt_2").eq(_index7).css("display","block").siblings("p.bt_2").css("display","none");
-	$(".imgTop img").eq(_index7).addClass("tm").siblings(".imgTop img").removeClass("tm");
+	$(this).stop().stop().animate({width:"1202"},1000).siblings("li").stop().animate({width:"179.5"},1000);
+  $(".imgCen").eq(_index7).css("display","block").siblings(".imgCen").css("display","none");
+  $("p.bt_2").eq(_index7).css("display","block").siblings("p.bt_2").css("display","none");
+  // $("p.bt_2").eq(_index7).css({width:"100%"}).siblings("p.bt_2").css({width:"0%"});
+  $(".imgTop div").eq(_index7).addClass("tm").parent().parent().siblings().find(".bg-bl").removeClass("tm");
+  console.log($(".imgTop .bg-bl").eq(_index7).addClass("tm").parent().parent().siblings());
+
 });
 $(".flash4 ul li").mouseout(function(){
 	$(".imgCen").css("display","none");
